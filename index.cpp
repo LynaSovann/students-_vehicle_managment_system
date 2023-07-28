9acb300c1c847b8c585a7e92c8cc6bdf080696e9
Reth-Sela <selareth7@gmail.com>
2023-07-28 19:57:46 +0700

    test

--- index.cpp
+++ index.cpp
@@ -77,6 +77,7 @@ void read_staff_file()
     if (!staffFile)
     {
         cout << "\tNo staff!!" << endl;
+        cout << " dfdjfdj";
     }
     string data;
     cout << "\t=================\tStaff table\t================" << endl;
